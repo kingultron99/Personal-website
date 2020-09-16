5b7c3b24c86101f20d15b70ca2b89299bd5d8ee9
lochlan <lochym10@gmail.com>
2020-09-16 22:49:25 +1000

    more content!

--- _data/user.js
+++ _data/user.js
@@ -1,12 +1,12 @@
 const fetch = require("node-fetch")
 
 module.exports = async () => {
-    const reposUrl = 'https://api.github.com/users/kingultron99/repos'
-    const repos = await fetch(reposUrl, {
+    const userUrl = 'https://api.github.com/users/kingultron99'
+    const user = await fetch(userUrl, {
         headers: {
             'Authorization': 'token 70b7230dc964e2b64cb16e9e5e934b5960aada71',
             'Accept': 'application/vnd.github.v3+json'
         }
     }).then(res => res.json())
-    return repos
+    return user
 }
