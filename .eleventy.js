4ae64e7d320f077f8d8a45f221d093a26f57aa31
lochlan <lochym10@gmail.com>
2020-10-22 14:55:18 +1100

    removed unused packages

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,5 @@
-
-const pluginSass = require("eleventy-plugin-sass");
-
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginSass, sassPluginOptions);
-};
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('js')
-  eleventyConfig.addPassthroughCopy('css')
   eleventyConfig.addPassthroughCopy('assets')
   return {
     passthroughFileCopy: true
