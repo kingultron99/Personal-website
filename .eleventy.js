55150a7e27dfe058258ab3393b10a548c5320ba0
lochlan <lochym10@gmail.com>
2020-10-22 15:11:40 +1100

    tiny changes were made....

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('js')
+  eleventyConfig.addPassthroughCopy('css')
   eleventyConfig.addPassthroughCopy('assets')
   return {
     passthroughFileCopy: true
